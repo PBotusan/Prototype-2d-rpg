16d887da02366909ad2f5732fc633822c64db278
Paul B <p_botusan@live.nl>
2021-01-12 17:43:14 +0100

    Scene trans

--- topdown rpg/Assets/Script/CameraDungeonController.cs
+++ topdown rpg/Assets/Script/CameraDungeonController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
+public class CameraDungeonController : MonoBehaviour
 {
     /// <summary>
     /// Gameobject of player used to move the player.
@@ -19,6 +19,16 @@ public class CameraController : MonoBehaviour
     /// </summary>
     [SerializeField] Vector3 offset;
 
+    [SerializeField] Vector2 maxPosition;
+    public Vector2 MaxPosition { get { return maxPosition; } set { maxPosition = value; } }
+
+    [SerializeField] Vector2 minPosition;
+    public Vector2 MinPosition { get { return minPosition; } set { minPosition = value; } }
+
+    private void Start()
+    {
+        // transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+    }
 
     /// <summary>
     /// Update is called once per frame
@@ -35,7 +45,14 @@ public class CameraController : MonoBehaviour
         // offst + the target position in gameworld.
         Vector3 desiredPosition = target.position + offset;
 
+        //clamp between min and max value, does not go boyond that.
+        desiredPosition.x = Mathf.Clamp(desiredPosition.x, minPosition.x, maxPosition.x);
+        desiredPosition.y = Mathf.Clamp(desiredPosition.y, minPosition.y, maxPosition.y);
+
+
         Vector3 smoothPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
         transform.position = smoothPosition;
+
+
     }
 }
---
+++ topdown rpg/Assets/Script/ChangeRoom.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeRoom : MonoBehaviour
+{
+    [SerializeField] Vector2 changeCameraMinPos;
+    [SerializeField] Vector2 changeCameraMaxPos;
+
+    [SerializeField] Vector3 changePlayerPos;
+
+    /// <summary>
+    /// Current Camera used as CameraController, to change the position.
+    /// </summary>
+    private CameraDungeonController cam;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main.GetComponent<CameraDungeonController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            cam.MinPosition += changeCameraMinPos;
+            cam.MaxPosition += changeCameraMaxPos;
+
+            Debug.Log("Player transistion");
+
+
+            collision.transform.position += changePlayerPos;
+        }
+    }
+}
