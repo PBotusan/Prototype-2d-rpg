f9ce6d594d239de71eae161723584211847ce360
Paul B <p_botusan@live.nl>
2021-01-21 09:51:36 +0100

    Observer objects
    Activate and deativate enemies en objects in world, only works if player is already in the world, needs some tweaking.
    
    started with scripts for enemies in dungeon room

---
+++ topdown rpg/Assets/Script/Room.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    /// <summary>
+    /// Restore enemies.
+    /// </summary>
+    [SerializeField] EnemyController[] enemies;
+
+    /// <summary>
+    /// Restore the Pots/Destructables.
+    /// </summary>
+    [SerializeField] BreakableObjectsController[] breakables;
+
+    /// <summary>
+    /// On collision enter, restore the enemies and destructables.
+    /// </summary>
+    /// <param name="collision"></param>
+    protected virtual void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && !collision.isTrigger)
+        {
+            //activate the enemies and destructables.
+            for (int i = 0; i < enemies.Length; i++)
+            {
+                ChangeActivation(enemies[i], true);
+            }
+
+            for (int i = 0; i < breakables.Length; i++)
+            {
+                ChangeActivation(breakables[i], true);
+            }
+        }
+
+    }
+
+    /// <summary>
+    /// When player leaves room deactivate enemies and breakables.
+    /// </summary>
+    /// <param name="collision"></param>
+    protected virtual void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && !collision.isTrigger)
+        {
+            //Deactivate the enemies and destructables.
+            for (int i = 0; i < enemies.Length; i++)
+            {
+                ChangeActivation(enemies[i], false);
+            }
+
+            for (int i = 0; i < breakables.Length; i++)
+            {
+                ChangeActivation(breakables[i], false);
+            }
+
+        }
+    }
+
+    /// <summary>
+    /// De/activate components for performance, and deactivates enemies/pots when entering other room.
+    /// </summary>
+    /// <param name="component"> The type you want to activate </param>
+    /// <param name="activation"> Activation based on true/false.</param>
+    void ChangeActivation(Component component, bool activation)
+    {
+        component.gameObject.SetActive(activation);
+    }
+}
