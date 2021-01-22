e0b250e4b142aca30f5c19a6a8b9417c98ab5ceb
Paul B <p_botusan@live.nl>
2021-01-22 14:02:19 +0100

    Reworked overworld camera

--- topdown rpg/Assets/Script/OverworldRoom.cs
+++ topdown rpg/Assets/Script/OverworldRoom.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class OverworldRoom : Room
 {
-    // Start is called before the first frame update
-    void Start()
+    //========================override Room==============================\\
+    //Change activation is in Room-class
+
+    protected virtual void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.CompareTag("Player") && !collision.isTrigger)
+        {
+            //activate the enemies and destructables.
+            for (int i = 0; i < enemies.Length; i++)
+            {
+                ChangeActivation(enemies[i], true);
+            }
+
+            for (int i = 0; i < breakables.Length; i++)
+            {
+                ChangeActivation(breakables[i], true);
+            }
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// When player leaves room deactivate enemies and breakables.
+    /// </summary>
+    /// <param name="collision"></param>
+    protected override void OnTriggerExit2D(Collider2D collision)
     {
-        
+        if (collision.CompareTag("Player") && !collision.isTrigger)
+        {
+            //Deactivate the enemies and destructables.
+            for (int i = 0; i < enemies.Length; i++)
+            {
+                ChangeActivation(enemies[i], false);
+            }
+
+            for (int i = 0; i < breakables.Length; i++)
+            {
+                ChangeActivation(breakables[i], false);
+            }
+            
+        }
     }
 }
---
+++ topdown rpg/Assets/Script/ScriptableObjects/LootTable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loot
+{
+    [SerializeField] Item Loot;
+    [SerializeField] int lootChange;
+
+}
+
+
+[CreateAssetMenu]
+public class LootTable : ScriptableObject
+{
+   
+}
