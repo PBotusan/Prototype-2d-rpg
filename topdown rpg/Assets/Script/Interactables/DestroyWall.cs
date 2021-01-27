613c5176b74d94020b090acb2f8a67321e39c429
Paul B <p_botusan@live.nl>
2021-01-27 16:41:01 +0100

    Bomb, New Scene, pos
    SecretCaveRoom
    Bomb destroys wall secret
    Destroywall scripts
    
    deleted some old code with camera values
    
    Fixed the player pos to 0 = better for new scene and easier to implement.

---
+++ topdown rpg/Assets/Script/Interactables/DestroyWall.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyWall : MonoBehaviour
+{
+    /// <summary>
+    /// used to disable the hint/sign.
+    /// </summary>
+    [SerializeField] GameObject hint;
+
+    /// <summary>
+    /// Enable entrance when bombed.
+    /// </summary>
+    [SerializeField] GameObject Entrance;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("BombExplosion") && collision.isTrigger)
+        {
+            hint.SetActive(false);
+            Entrance.SetActive(true);
+        }
+    }
+}
--- topdown rpg/Assets/Script/SceneTransition.cs
+++ topdown rpg/Assets/Script/SceneTransition.cs
@@ -12,16 +12,16 @@ public class SceneTransition : MonoBehaviour
     [SerializeField] Vector2 playerPos;
     [SerializeField] VectorValueOfPlayer playerStorage;
 
-    [SerializeField] Vector2 cameraNewMinPos;
-    [SerializeField] VectorValueOfPlayer cameraMinPos;
-
-    [SerializeField] Vector2 cameraNewMaxPos;
-    [SerializeField] VectorValueOfPlayer cameraMaxPos;
+    /// <summary>
+    /// old playerpostion shows the position the player first entered the building. 
+    /// </summary>
+    [SerializeField] VectorValueOfPlayer playerPosOldValue;
 
 
     [SerializeField] GameObject fadeInPanel;
     [SerializeField] GameObject fadeOutPanel;
     [SerializeField] float fadeTime;
+
 
     //todo dungeon fading
     //todo cave fading
@@ -39,17 +39,11 @@ public class SceneTransition : MonoBehaviour
         }
     }
 
-    /// <summary>
-    /// old playerpostion shows the position the player first entered the building. 
-    /// </summary>
-    [SerializeField] VectorValueOfPlayer playerPosOldValue;
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player") && !collision.isTrigger) 
         {
             playerPosOldValue.initialValue = playerPos;
-            //SceneManager.LoadScene(loadScene);
             StartCoroutine(FadeCoroutine());
 
         }
@@ -64,8 +58,7 @@ public class SceneTransition : MonoBehaviour
         }
 
         yield return new WaitForSeconds(fadeTime);
-        //reset camera pos
-        ResetCameraBounds();
+
 
         //fade after loading
         AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(loadScene);
@@ -76,12 +69,5 @@ public class SceneTransition : MonoBehaviour
         }
     }
 
-    /// <summary>
-    /// Reset the camera position to new value position.
-    /// </summary>
-    public void ResetCameraBounds()
-    {
-        cameraMaxPos.initialValue = cameraNewMaxPos;
-        cameraMinPos.initialValue = cameraNewMinPos;
-    }
+   
 }
