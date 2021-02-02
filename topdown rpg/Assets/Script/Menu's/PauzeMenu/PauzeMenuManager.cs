b52db34d57e639c94cf3b8230dbd1fa081c36b30
Paul B <p_botusan@live.nl>
2021-02-02 10:58:49 +0100

    Pause menu

---
+++ topdown rpg/Assets/Script/Menu's/PauzeMenu/PauzeMenuManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauzeMenuManager : MonoBehaviour
+{
+    [SerializeField] GameObject pausePanel;
+    [SerializeField] string mainMenu;
+
+    private bool isPaused;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isPaused = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Pause"))
+        {
+            CheckIfPaused();
+        }
+    }
+
+    public void CheckIfPaused()
+    {
+        isPaused = !isPaused;
+
+        // lazy quick way
+        if (isPaused)
+        {
+            pausePanel.SetActive(true);
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            pausePanel.SetActive(false);
+            Time.timeScale = 1f;
+        }
+    }
+
+
+    public void Quit()
+    {
+         Time.timeScale = 1;
+        SceneManager.LoadScene(mainMenu);
+    }
+}
--- topdown rpg/Assets/Script/Menu's/TitleScreen/Titlescreen.cs
+++ topdown rpg/Assets/Script/Menu's/TitleScreen/Titlescreen.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine.SceneManagement;
-
 using UnityEngine;
 
 public class Titlescreen : MonoBehaviour
