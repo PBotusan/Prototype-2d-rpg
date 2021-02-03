c70e80a014d3529e30908a410053022a144e6f65
Paul B <p_botusan@live.nl>
2021-02-03 12:06:23 +0100

    UI for inventory, scriptable-objects for inventory

---
+++ topdown rpg/Assets/Script/ScriptableObjects/Inventory/InventoryItem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Items")]
+public class InventoryItem : ScriptableObject
+{
+    [SerializeField] string itemName;
+    [SerializeField] string itemDescription;
+    [SerializeField] Sprite itemImage;
+    [SerializeField] int numberHold;
+    [SerializeField] bool usable;
+    [SerializeField] bool unique;
+
+    public string ItemName { get { return itemName; } set { itemName = value; } }
+    public string ItemDescription { get { return itemDescription; } set { itemDescription = value; } }
+    public Sprite ItemImage { get { return itemImage; } set { itemImage = value; } }
+    public int NumberHold { get { return numberHold; } set { numberHold = value; } }
+    public bool Usable { get { return usable; } set { usable = value; } }
+    public bool Unique { get { return unique; } set { unique = value; } }
+}
