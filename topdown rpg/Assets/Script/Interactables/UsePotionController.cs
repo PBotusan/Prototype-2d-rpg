b7060b8bc3ae58f644af306d1480422c89547e34
Paul B <p_botusan@live.nl>
2021-02-04 11:36:29 +0100

    consumables potions
    use potions,
    heal,
    inventory updates

---
+++ topdown rpg/Assets/Script/Interactables/UsePotionController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UsePotionController : MonoBehaviour
+{
+    [SerializeField] FloatValue playerStamina;
+    [SerializeField] FloatValue heartContainers;
+    [SerializeField] FloatValue playerHealth;
+
+    [SerializeField] SignalSender staminaSignal;
+    [SerializeField] SignalSender healthSignal;
+
+
+    public void RedPotion(int amountHealth)
+    {
+        var maxHealth = heartContainers.RuntimeValue * 2;
+
+        playerHealth.RuntimeValue += amountHealth;
+        healthSignal.Raise();
+
+        if (playerHealth.RuntimeValue > maxHealth)
+        {
+            playerHealth.RuntimeValue = maxHealth;
+        }
+
+    }
+
+    public void GreenPotion(int amountHealth)
+    {
+        var maxHealth = heartContainers.RuntimeValue * 2;
+
+        playerStamina.RuntimeValue += 100;
+        playerHealth.RuntimeValue += amountHealth;
+        staminaSignal.Raise();
+
+        if (playerHealth.RuntimeValue > maxHealth)
+        {
+            playerHealth.RuntimeValue = maxHealth;
+        }
+
+    }
+}
