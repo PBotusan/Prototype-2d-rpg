ce16cd83d155a41e66634fda63a5ca09eddb3886
Paul B <p_botusan@live.nl>
2021-02-17 14:35:47 +0100

    FMS
    fms scrptable object for idle, and blueprints for the abstract class

---
+++ topdown rpg/Assets/Script/FiniteStateMachine/IdleState.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="IdleState", menuName = "FiniteStateMachine/States/Idle", order =1)]
+public class IdleState : AbstractFSMState
+{
+    public override bool EnterState()
+    {
+        base.EnterState();
+
+        Debug.Log("Enter idle state");
+
+        return true;
+    }
+
+    public override void UpdateState()
+    {
+        Debug.Log("Update IdleState");
+    }
+
+    public override bool ExitState()
+    {
+        base.ExitState();
+        Debug.Log("Exit IdleState");
+        return true;
+    }
+}
