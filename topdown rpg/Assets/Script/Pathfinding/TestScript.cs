28f997b008cb9ea96ce826f31ac2214f60f842eb
Paul B <p_botusan@live.nl>
2021-02-19 14:22:37 +0100

    Grid
    Grid for that can be true or false, stores value. use for pathfinding. does support multiple grids and is very flexable

--- topdown rpg/Assets/Script/Pathfinding/TestScript.cs
+++ topdown rpg/Assets/Script/Pathfinding/TestScript.cs
@@ -1,18 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using CodeMonkey.Utils;
 
 public class TestScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Grid grid;
+
+    private void Start()
     {
-        
+        grid = new Grid(4, 2, 10f, new Vector3(20,0));
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            grid.SetValue(UtilsClass.GetMouseWorldPosition(), 56 );
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log(grid.GetValue(UtilsClass.GetMouseWorldPosition()));
+        }
     }
 }
